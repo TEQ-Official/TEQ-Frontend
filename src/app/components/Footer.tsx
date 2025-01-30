6018ab71eaec23be07230e92a17641dd44ea1236
Okeke Chioma Princess <okekegeraldenne@gmail.com>
2025-01-30 14:58:15 +0100

    Finished footer section

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -54,23 +54,28 @@ const links = [
 function Footer() {
     return (
         <footer className="bg-[#000E22] text-white">
-            <div className="flex gap-10 pl-[80px] pr-[52px]">
-                <div>
+            <div className="flex gap-10 pl-20 pr-[52px] pt-20 pb-6">
+                <div className="flex flex-col gap-3">
                     <Logo color="white" />
-                    <p className="w-[323px]">
+                    <p className="w-[323px] text-[#98A2B3]">
                         Join us on this journey as we hel you refine your
                         english knowledge
                     </p>
                 </div>
                 <div className="flex-1">
                     <div className="flex flex-col justify-between h-full gap-10 divide-y divide-white/20 ">
-                        <div className="flex gap-[49.34px]">
+                        <div className="flex justify-between">
                             <div className="grid grid-cols-3 gap-[49.34px]">
                                 {links.map((link, index) => {
                                     return (
-                                        <div key={index} className="flex flex-col gap-10">
-                                            <p>{link.title}</p>
-                                            <ul className="flex flex-col gap-10">
+                                        <div
+                                            key={index}
+                                            className="flex flex-col gap-10"
+                                        >
+                                            <p className="font-semibold text-xl">
+                                                {link.title}
+                                            </p>
+                                            <ul className="flex flex-col gap-10 text-[#98A2B3]">
                                                 {link.subLinks.map(
                                                     (subLink, linkIndex) => {
                                                         return (
@@ -93,18 +98,20 @@ function Footer() {
                                 })}
                             </div>
                             <div className="flex flex-col gap-5">
-                                <p>Reach us</p>
-                                <div>
-                                <p>
-                                    2972 Westheimer Rd. Santa Ana, Illinois
-                                    85486{" "}
+                                <p className="text-xl font-semibold">
+                                    Reach us
                                 </p>
-                                <div></div>
+                                <div>
+                                    <p className="text-[#98A2B3]">
+                                        2972 Westheimer Rd. Santa Ana, Illinois
+                                        85486{" "}
+                                    </p>
+                                    <div></div>
                                 </div>
                             </div>
                         </div>
-                        <div className="flex justify-between pt-[22px]">
-                            <p>
+                        <div className="flex justify-between pt-[22px] text-[#D0D5DD]">
+                            <p className="">
                                 Copyright 2025 TheenglishCliniq.io. All Rights
                                 Reserved
                             </p>
