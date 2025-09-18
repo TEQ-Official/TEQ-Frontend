416c291ec54c909b5da713d1ac81ba58921ad4dd
Chux-Dennis <chuksabosi63@gmail.com>
2025-09-18 10:59:56 +0100

    Changes to onboarding page

--- src/components/home/SmallTestimonyCard.tsx
+++ src/components/home/SmallTestimonyCard.tsx
@@ -4,7 +4,7 @@ import { BsStarFill } from "react-icons/bs";
 export default function SmallTestimonyCard ({rotate}:{rotate : string}){
         return (
             <div className={`hidden absolute -left-20 top-10 text-left text-sm lg:w-[300px] bg-white rounded-2xl shadow-lg lg:flex items-start ${rotate}`}>
-                    <Image width={1000} height={1000} src="/freepik.png" alt="freepik" className="w-36 h-36 rounded object-cover" />
+                    <Image width={1000} height={1000} src="/freepik.png" alt="Tutor Image" className="w-36 h-36 rounded object-cover" />
                     <div className="p-5 flex justify-between flex-col  gap-5 text-xs">
                         <p className="text-gray-700">
                             The English clinic truly serves to cure your spoken and written English
--- src/components/onboarding/TestCard.tsx
+++ src/components/onboarding/TestCard.tsx
@@ -9,13 +9,13 @@ export default function TestCard({ title, description, selected, onClick }: Test
     return (
         <button
             onClick={onClick}
-            className={`w-full h-40 text-left p-4 rounded-md transition shadow-[#c3ccd7] shadow-sm border border-[#c3ccd7] flex flex-col justify-between ${selected ? 'bg-[#f1f5f9]' : 'bg-white'}`}
+            className={`w-full h-40 text-left p-4 rounded-md transition shadow-[#c3ccd7] shadow-sm border border-[#c3ccd7] flex hover:bg-[#caddfe]/30 flex-col justify-between ${selected ? 'bg-[#caddfe]' : 'bg-white'}`}
         >
             <span className="flex justify-between items-center">
                 <h3 className="font-semibold text-sm uppercase">{title}</h3>
                 <span className={`w-5 h-5 rounded-full border-2 flex items-center justify-center ${selected ? 'border-[#0047AB]' : 'border-gray-300'}`}>{selected && <span className="w-2.5 h-2.5 bg-[#0047AB] rounded-full" />}</span>
             </span>
-            <p className="text-gray-600">{description}</p>
+            <p className="text-gray-600 text-[10px]  sm:text-[14px] md:text-[1.1rem]">{description}</p>
         </button>
     );
 }
